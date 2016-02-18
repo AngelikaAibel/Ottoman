bafe3b26257cf17ae7f3501c19e16754f27c8c35
Angie <angelika.aibel@gmail.com>
2016-02-18 21:15:33 +0000

    Lets try this

---
+++ getHierarchy_new.py
@@ -0,0 +1,52 @@
+
+#script to transverse hierarchy
+#http://www.cineversity.com/forums/viewthread/1307/
+
+def GetAllObjects(obj, objList):
+    if obj is None: return
+
+    #Actions can go here
+    objList.append(obj)
+    #End Actions
+    if (obj.GetDown()):
+        GetAllObjects(obj.GetDown(), objList)
+    if (obj.GetNext()):
+        GetAllObjects(obj.GetNext(), objList)
+    return objList
+
+def GoDownHierarchy(obj, stop, objList):
+    if obj is None: return
+    if obj==stop: return
+    
+    #Actions can go here
+    objList.append(obj)
+    #End Actions
+    if (obj.GetDown()):
+        GoDownHierarchy(obj.GetDown(), stop, objList)
+    if (obj.GetNext()):
+        GoDownHierarchy(obj.GetNext(), stop, objList)
+
+    return objList
+
+
+def main():
+    emptyList=[]
+    allObjs=GetAllObjects(doc.GetFirstObject(), emptyList)
+    null=None
+    for obj in allObjs:
+        name=obj.GetName()
+        if len(name.split('_')>0:
+            if name.split('_')[-1]=='cpk':
+                null=obj
+                break
+
+    if null != None:
+        emptyList2=[]
+        children=GoDownHierarchy(null, null.GetNext(), emptyList2)
+
+    if len(children)>0:
+        for id, child in children:
+            if id==0:
+                doc.SetSelection(children, c4d.SELECTION_NEW)
+            else:
+                doc.SetSelection(children, c4d.SELECTION_ADD) 
