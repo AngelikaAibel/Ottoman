2dc8cfedbe57abdcaffc9afec07e58a805df8a12
Angie <angelika.aibel@gmail.com>
2016-02-19 22:56:26 +0000

    Look for character component tags and recurse hierachy

--- transfer_animation.py
+++ transfer_animation.py
@@ -1,44 +1,26 @@
-#can go through hierarchy perhaps, but need a way to store the found controllers in a recursive function(global variable??)
-
 import c4d
 from c4d import gui
-#Welcome to the world of Python
 
 def copy_animation(source, target):
-    
 
- #   doc.SetSelection(source, c4d.SELECTION_NEW) 
- #   doc.SetSelection(target, c4d.SELECTION_ADD)  
-
-
-   # print "Source: %s \n" %source
-   # print "Target: %s \n" %target
-
-
-    # look if there is already a track
+    # look if there is a track
     tracks = source.GetCTracks()
 
-   # print "Track: %s \n" %tracks
-
-      # if not create the track
+    # if not create the track
     if tracks == None:
       print "Sorry - no tracks found \n"
 
     for track in tracks:
-        #print "Done\n"
         curve = track.GetCurve()
         target.InsertTrackSorted(track.GetClone())
 
 def recurse_hierarchy(op, store_controllers):
     
-    
     while op:
-        
-        #print "Object working on: %s" %op
-        object_name = op.GetName()
-        if op.GetType() == 5101:
-            #print op
-            store_controllers.append(op)
+        #Check if there is a character controller tag for object => then it is a controller
+        for i in op.GetTags():
+            if i.GetType() == 1022113:
+               store_controllers.append(op)
         
         recurse_hierarchy(op.GetDown(),store_controllers)
         op = op.GetNext()
@@ -46,18 +28,16 @@ def recurse_hierarchy(op, store_controllers):
     return store_controllers
 
 def main():
-    gui.MessageDialog('Hello World!')
-
+    #get active objects
     object = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_SELECTIONORDER)
     
     source = object[0]
     target = object[1]   
-   # print source
-   # print source.GetName()
-   
+
     store_source_controllers = []
     store_target_controllers = []
 
+    #find controllers
     if doc:
         source_controllers = recurse_hierarchy(source, store_source_controllers)
         target_controllers = recurse_hierarchy(target, store_target_controllers)
@@ -65,14 +45,12 @@ def main():
 
     print "ControllerList length is: %s \n" %str(len(source_controllers))
 
-    print source_controllers
-
     if len(source_controllers) == len(target_controllers):
        for s_con,t_con in zip(source_controllers, target_controllers):
           # print "%s - %s \n" %(source,target)    
            copy_animation(s_con, t_con)
     else:
-        print "Sorry \n"
+        print "Sorry there has been an error - the rig hierarchies are not the same\n"
 
         
 if __name__=='__main__':
